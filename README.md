c2f788add434ceda3514bad502896b1b2468c1ba
Maksim Kostromin <daggerok@gmail.com>
2018-11-24 06:40:11 +0200

    First Blood: JBoss EAP 6.4.0 docker automation build.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# jboss-eap-6.4
-JBoss EAP 6.4
+# jboss-eap-6.4 [![Build Status](https://travis-ci.org/daggerok/jboss-eap-6.4.svg?branch=master)](https://travis-ci.org/daggerok/jboss-eap-6.4)
+Docker hub JBoss EAP 6.4 automation build based on centos7 images
+
+[daggerok/jboss-eap-6.4](https://hub.docker.com/r/daggerok/jboss-eap-6.4/)
+
+## tags
+
+- [latest](https://github.com/daggerok/jboss-eap-6.4/blob/master/Dockerfile)
+<!-- - [6.4.0](https://github.com/daggerok/jboss-eap-6.4/blob/master/Dockerfile) -->
+
+## usage
+
+### health check
+
+```Dockerfile
+
+FROM daggerok/jboss-eap-6.4
+HEALTHCHECK --timeout=2s --retries=22 \
+        CMD wget -q --spider http://127.0.0.1:8080/my-service/health \
+         || exit 1
+ADD ./target/*.war ${JBOSS_HOME}/standalone/deployments/my-service.war
+
+```
+
+### multi deployment
+
+```Dockerfile
+
+FROM daggerok/jboss-eap-6.4:6.4.0
+COPY ./build/libs/*.war ./target/*.war ${JBOSS_HOME}/standalone/deployments/
+
+```
+
+### remote debug
+
+```Dockerfile
+
+FROM daggerok/jboss-eap-6.4:latest
+ENV JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "
+EXPOSE 5005
+COPY ./target/*.war ${JBOSS_HOME}/standalone/deployments/
+
+```
+
+## ports
+
+- management: 9990, 9999
+- web http: 8080
+- https: 8443
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#####################################################################################################
+# make sure your system has netstat package installed for JBoss healthcheck and this script working #
+# also do not forget to add ${JBOSS_HOME}/bin and /tmp folders to your ${PATH} environment variable #
+#####################################################################################################
+
+function waiting_for_jboss {
+  for port in "9990" "9999" ; do
+    while [[ $(sudo netstat -ltnp | grep ${port} | wc -l) -lt 1 ]] ; do sleep 1s ; done
+  done
+}
+
+waiting_for_jboss
+
+if [ "${JBOSS_EAP_PATCH}" != '6.4.0' ] ; then
+
+  mkdir -p ./patches
+  wget -q -i "${PATCHES_BASE_URL}/index.txt" -P ./patches/ || echo 'no patches found.'
+
+  for PATCH_FILENAME in $(ls ./patches/*.zip) ; do
+    echo "Applying $(basename ${PATCH_FILENAME}) patch..."
+    INSTRUCTIONS_FILE="${PATCH_FILENAME}.instructions"
+
+    if [[ ${PATCH_FILENAME} == *-6.4.14.CP.zip ]] ; then
+      echo "connect
+            patch apply ${PATCH_FILENAME} --override=bin/standalone.conf,bin/standalone.conf.bat
+            shutdown --restart=true" > ${INSTRUCTIONS_FILE}
+    else
+      echo "connect
+            patch apply ${PATCH_FILENAME}
+            shutdown --restart=true" > ${INSTRUCTIONS_FILE}
+    fi
+
+    jboss-cli.sh --file=${INSTRUCTIONS_FILE}
+    rm -rf ${INSTRUCTIONS_FILE} ${PATCH_FILENAME}
+    waiting_for_jboss
+  done
+fi
+
+if [ -z ${KEEP_HISTORY} ] || [[ ! ${KEEP_HISTORY} =~ ^(keep|yes|true)$ ]] ; then
+  echo 'Cleanup history...'
+  jboss-cli.sh --commands='connect','/core-service=patching:ageout-history'
+fi
+
+echo 'Shutdown JBoss...'
+jboss-cli.sh --commands='connect','shutdown --restart=false'
+
+echo 'Done.'
